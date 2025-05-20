07afebcbd2a45bf61ea761375d61f43ab9d172a8
luciano giron <lucianogiron110@gmail.com>
2025-05-20 02:16:26 -0300

    Modificar producto Luciano Giron

--- src/App.jsx
+++ src/App.jsx
@@ -7,10 +7,18 @@ import SearchBar from './componentes/SearchBar.jsx';
 function App() {
   const [productos, setProductos] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
-  
+  const [producto_editar, setProducto_editar] = useState('');
 
   const agregarProducto = useCallback((nuevoProducto) => {
     const existe = productos.some(prod => prod.id === nuevoProducto.id);
+
+    if (producto_editar) // entra si la variable tiene un valor y actualiza
+    {
+      modificar_producto(nuevoProducto);
+      setProducto_editar(''); //limpia
+      return;
+    }
+
     if (existe) {
       alert('Ese ID ya existe en otro producto');
       return;
@@ -22,29 +30,35 @@ function App() {
     };
 
     setProductos(prevProductos => [...prevProductos, productoConDescuento]);
-  }, [productos]);
+  }, [productos, producto_editar]);
 
   const eliminarProducto = useCallback((id) => {
     setProductos(prev => prev.filter(prod => prod.id !== id));
   }, []);
 
-  const modificarProducto = useCallback((productoActualizado) => {
-    setProductos(prev =>
-      prev.map(prod => (prod.id === productoActualizado.id ? productoActualizado : prod))
+  const modificar_producto = useCallback((productoActualizado) => { //recibe productoActualiado que es el producto con los nuevos datos a guardar
+    const productoConDescuento = {  //para que el prod tenga un nuevo precio
+      ...productoActualizado,
+      precioConDescuento: productoActualizado.precioUnitario * (1 - productoActualizado.descuento / 100),
+    };
+
+    setProductos(prevProductos =>
+      prevProductos.map(producto => //recorre el arreglo
+        producto.id === productoActualizado.id // compracion del id de prod con el que queremos editar
+          ? productoConDescuento // reemplaza si es el que se está actualizando
+          : producto // si no, lo deja igual
+      )
     );
   }, []);
 
-const productosFiltrados = useMemo(() => {
-  return productos.filter(producto => {
-    const termino = searchTerm.toLowerCase();
-    const descripcion = producto.descripcion?.toLowerCase() ?? '';
-    const id = String(producto.id).toLowerCase();
-
-    return descripcion.includes(termino) || id.includes(termino);
-  });
-}, [productos, searchTerm]);
-
-
+  const productosFiltrados = useMemo(() => {
+    return productos.filter(producto => {
+      const termino = searchTerm.toLowerCase();
+      const descripcion = producto.descripcion?.toLowerCase() ?? '';
+      const id = String(producto.id).toLowerCase();
+      return descripcion.includes(termino) || id.includes(termino);
+    });
+  }, [productos, searchTerm]);
 
   useEffect(() => {
     console.log('Lista de productos actualizada:', productos);
@@ -53,8 +67,8 @@ const productosFiltrados = useMemo(() => {
   return (
     <div className="app-container">
       <SearchBar searchTerm={searchTerm} onSearchChange={setSearchTerm} />
-      <ProductForm onGuardar={agregarProducto} />
-      <ProductList productos={productosFiltrados} onEliminar={eliminarProducto} onEditar={modificarProducto} />
+      <ProductForm onGuardar={agregarProducto} editingProduct={producto_editar} />
+      <ProductList productos={productosFiltrados} onEliminar={eliminarProducto} onEditar={setProducto_editar} />
     </div>
   );
 }
